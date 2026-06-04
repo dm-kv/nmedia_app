7b60497ff7fcffbbf6d8ac60415de6a8b4cff71b
Dm Kv <demon1st@yandex.ru>
2026-06-05 00:23:13 +0300

    threads

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -11,7 +11,6 @@ import ru.netology.nmedia.model.FeedModel
 import ru.netology.nmedia.repository.PostRepositoryNetworkImpl
 import kotlin.concurrent.thread
 
-
 private val empty = Post()
 class PostViewModel(application: Application): AndroidViewModel(application) {
 
