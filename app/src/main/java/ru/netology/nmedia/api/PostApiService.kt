5b9895dbc681a9de9ace096ae77fcbf813b03036
Dm Kv <demon1st@yandex.ru>
2026-06-14 22:23:46 +0300

    api

---
+++ app/src/main/java/ru/netology/nmedia/api/PostApiService.kt
@@ -0,0 +1,55 @@
+package ru.netology.nmedia.api
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import android.R.attr.level
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import com.google.ai.client.generativeai.BuildConfig
+import ru.netology.nmedia.dto.Post
+import java.util.concurrent.TimeUnit
+
+private const val BASE_URL = "http://10.0.2.2:9999/api/slow/"
+
+private val client = OkHttpClient.Builder()
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .client(client)
+    .build()
+
+interface PostApiService {
+    @GET("posts")
+    fun get(): Call<List<Post>>
+
+    @GET("posts/{id}")
+    fun getById(@Path("id") id: Long): Call<Post>
+
+    @POST("posts")
+    fun save(@Body post: Post): Call<Post>
+
+    @DELETE("posts/{id}")
+    fun removeById(@Path("id") id: Long): Call<Unit>
+
+    @POST("posts/{id}/likes")
+    fun likeById(@Path("id") id: Long): Call<Post>
+
+    @DELETE("posts/{id}/likes")
+    fun dislikeById(@Path("id") id: Long): Call<Post>
+}
+
+object PostApi{
+    val service: PostApiService by lazy {
+        retrofit.create()
+    }
+}
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryNetworkImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryNetworkImpl.kt
@@ -1,109 +1,141 @@
 package ru.netology.nmedia.repository
 
-
-import com.google.gson.Gson
-import java.util.concurrent.TimeUnit
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import ru.netology.nmedia.api.PostApi
 import ru.netology.nmedia.dto.Post
-import okhttp3.Call
-import com.google.gson.reflect.TypeToken
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.RequestBody
+
 
 class PostRepositoryNetworkImpl: PostRepository {
-    private companion object{
-        const val BASE_URL = "http://10.0.2.2:9999/api/slow/"
-        val jsonType = "application/json".toMediaType()
-    }
-
-    private val client = OkHttpClient.Builder()
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .build()
-
-    private val gson = Gson()
-    private  val postType = object : TypeToken<List<Post>>()  {}.type
 
     override fun get(): List<Post> {
-        val request: Request = Request.Builder()
-            .url("${BASE_URL}posts")
-            .build()
+        return PostApi.service.get()
+            .execute()
+            .let {
+                if (it.isSuccessful) {
+                    it.body() ?: throw RuntimeException("body is null")
+                } else {
+                    throw RuntimeException(it.message())
+                }
 
-        val call:Call = client.newCall(request)
+            }
 
-        val response = call.execute()
-
-        return gson.fromJson(response.body.string(), postType)
     }
 
-    override fun likeById(id: Long) {
-        val currentPosts = get()
-        val post = currentPosts.find { it.id == id }
+    override fun getAsync(callback: PostRepository.PostCallback<List<Post>>) {
+        PostApi.service.get().enqueue(object : Callback<List<Post>> {
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                if (!response.isSuccessful) {
+                    when (response.code()) {
+                        404 -> callback.onError(RuntimeException("Post not found"))
+                        500 -> callback.onError(RuntimeException("Server error"))
+                        else -> callback.onError(RuntimeException("Error: ${response.code()}"))
+                    }
+                    return
+                }
 
-        if (post?.likedByMe == true) {
-            removeLike(id)
+                val body = response.body()
+                if (body == null) {
+                    callback.onError(RuntimeException("body is null"))
+                } else {
+                    callback.onSuccess(body)
+                }
+            }
+
+            override fun onFailure(call: Call<List<Post>>, e: Throwable) {
+                callback.onError(e as Exception)
+            }
+        })
+    }
+
+
+
+    override fun likeById(
+        id: Long,
+        likedByMe: Boolean,
+        callback: PostRepository.PostCallback<Post>
+    ) {
+        val call = if (likedByMe) {
+            PostApi.service.dislikeById(id)
         } else {
-            addLike(id)
+            PostApi.service.likeById(id)
         }
+
+        call.enqueue(object : Callback<Post> {
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                if (!response.isSuccessful) {
+                    when (response.code()) {
+                        404 -> callback.onError(RuntimeException("Post not found"))
+                        500 -> callback.onError(RuntimeException("Server error"))
+                        else -> callback.onError(RuntimeException("Error: ${response.code()}"))
+                    }
+                    return
+                }
+
+                val body = response.body()
+                if (body == null) {
+                    callback.onError(RuntimeException("body is null"))
+                } else {
+                    callback.onSuccess(body)
+                }
+            }
+
+            override fun onFailure(call: Call<Post>, e: Throwable) {
+                callback.onError(e as Exception)
+            }
+        })
     }
-
-
-    private fun addLike(id: Long): Post {
-        val request: Request = Request.Builder()
-            .url("${BASE_URL}posts/$id/likes")
-            .post(RequestBody.create(null, ""))
-            .build()
-
-        val call: Call = client.newCall(request)
-        val response = call.execute()
-
-        return gson.fromJson(response.body?.string(), Post::class.java)
-    }
-
-    private fun removeLike(id: Long): Post {
-        val request: Request = Request.Builder()
-            .url("${BASE_URL}posts/$id/likes")
-            .delete()
-            .build()
-
-        val call: Call = client.newCall(request)
-        val response = call.execute()
-
-        return gson.fromJson(response.body?.string(), Post::class.java)
-    }
-
-
 
     override fun shareById(id: Long) {
-        TODO("Not yet implemented")
+
     }
 
-    override fun removeById(id: Long) {
-        val request: Request = Request.Builder()
-            .url("${BASE_URL}posts/$id")
-            .delete()
-            .build()
+    override fun save(post: Post, callback: PostRepository.PostCallback<Post>) {
+        PostApi.service.save(post)
+            .enqueue(object : Callback<Post> {
+                override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                    if (!response.isSuccessful) {
+                        when (response.code()) {
+                            404 -> callback.onError(RuntimeException("Post not found"))
+                            500 -> callback.onError(RuntimeException("Server error"))
+                            else -> callback.onError(RuntimeException("Error: ${response.code()}"))
+                        }
+                        return
+                    }
 
-        val call:Call = client.newCall(request)
+                    val body = response.body()
+                    if (body == null) {
+                        callback.onError(RuntimeException("body is null"))
+                    } else {
+                        callback.onSuccess(body)
+                    }
+                }
 
-        call.execute()
+                override fun onFailure(call: Call<Post>, e: Throwable) {
+                    callback.onError(e as Exception)
+                }
+            })
     }
 
-    override fun save(post: Post): Post {
-        val request: Request = Request.Builder()
-            .url("${BASE_URL}posts")
-            .post(
-                gson.toJson(post).toRequestBody(
-                jsonType),
-                )
-            .build()
+    override fun removeById(id: Long, callback: PostRepository.PostCallback<Unit>) {
+        PostApi.service.removeById(id)
+            .enqueue(object : Callback<Unit> {
+                override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
+                    if (!response.isSuccessful) {
+                        when (response.code()) {
+                            404 -> callback.onError(RuntimeException("Post not found"))
+                            500 -> callback.onError(RuntimeException("Server error"))
+                            else -> callback.onError(RuntimeException("Error: ${response.code()}"))
+                        }
+                        return
+                    }
+                    callback.onSuccess(Unit)
+                }
 
-        val call:Call = client.newCall(request)
-
-        val response = call.execute()
-
-        return gson.fromJson(response.body.string(), Post::class.java)
+                override fun onFailure(call: Call<Unit>, e: Throwable) {
+                    callback.onError(e as Exception)
+                }
+            })
     }
 }
-
